be5793e2c497dc32bd290a890d3c41e6c5f384df
johnnynode <johnnynode@gmail.com>
2017-03-22 18:17:06 +0800

    添加注释和总结

--- src/day02/Number/First.java
+++ src/day02/Number/First.java
@@ -1,9 +1,9 @@
 package day02.Number;
 
 public class First {
-	static String s1 = "你好";
+	static String s1 = "你好"; // 全局变量
 	public static void main(String[] args) {
-		String s2 = "Java";
+		String s2 = "Java"; // 局部变量
 		System.out.println(s1); // 你好
 		System.out.println(s2); // Java
 	}
