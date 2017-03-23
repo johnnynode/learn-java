814f162fcd0187a5384f30067b50a756ca623242
johnnynode <johnnynode@gmail.com>
2017-03-23 08:45:21 +0800

    add char code

---
+++ src/day02/Number/Gess.java
@@ -0,0 +1,13 @@
+package day02.Number;
+
+public class Gess {
+
+	public static void main(String[] args) {
+		char word = 'd', word2 = '@';
+		int p = 23045, p2 = 45213;
+        System.out.println("d 在unicode 表中的位置是：" + (int)word);
+        System.out.println("@ 在unicode 表中的位置是：" + (int)word2);
+        System.out.println("unicode 表中第23045位是：" + (char)p);
+        System.out.println("unicode 表中第45213位是：" + (char)p2);
+	}
+}
