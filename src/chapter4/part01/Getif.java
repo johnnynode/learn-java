bd2ac7162ddb6d03b559de9ff521237afb8389e6
johnnynode <johnnynode@gmail.com>
2017-04-01 12:49:05 +0800

    add GetIf Class

---
+++ src/chapter4/part01/Getif.java
@@ -0,0 +1,17 @@
+package chapter4.part01;
+
+public class Getif {
+
+	public static void main(String[] args) {
+		int x = 45;
+		int y = 12;
+		if(x>y){
+			System.out.println("变量x大于变量y");
+		}else if(x<y){
+			System.out.println("变量x小于变量y");
+		}else{
+			System.out.println("变量x=变量y");
+		}
+	}
+
+}
