f3f68a1ee0ca308b11ebd31c678a3eb0978e76c1
johnnynode <johnnynode@gmail.com>
2017-04-03 21:03:54 +0800

    添加Circulate类用于测试for循环

---
+++ src/chapter4/part02/Circulate.java
@@ -0,0 +1,15 @@
+package chapter4.part02;
+
+public class Circulate {
+	public static void main(String args[]){
+		testFor();
+	}
+	
+	public static void testFor(){
+		int sum = 0;
+		for(int i=2; i<=100; i+=2){
+			sum +=i;
+		}
+		System.out.println("2到100之间所有偶数之和为： " + sum);
+	}
+}
