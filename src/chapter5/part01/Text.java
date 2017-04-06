3b0edf74bdc1f3256071f11539182911327bc3f1
johnnynode <johnnynode@gmail.com>
2017-04-06 09:30:04 +0800

    整理

--- src/chapter5/part01/Text.java
+++ src/chapter5/part01/Text.java
@@ -1,7 +1,7 @@
 package chapter5.part01;
 
 public class Text {
-
+	static String str = "We are students";
 	public static void main(String[] args) {
 		testLength();
 		testIndexOf();
@@ -10,21 +10,18 @@ public class Text {
 
 	// 输出字符串的长度
 	public static void testLength() {
-		String str = "We are students";
 		int size = str.length(); // 注意String类型的长度是方法。
 		System.out.println("testLength: " + size); // testLength: 15
 	}
 
 	// 输出某字符索引位置
 	public static void testIndexOf() {
-		String str = "We are students";
 		int index = str.indexOf('a');
 		System.out.println("testIndexOf: " + index); // testIndexOf: 3
 	}
 
-	// 倒序输出某字符串位置
+	// 倒序输出某字符串位置 输出的还是索引，只是从后(右)开始查找
 	public static void testLastIndexOf() {
-		String str = "We are students";
 		int index = str.lastIndexOf('e');
 		System.out.println("testLastIndexOf: " + index); // testLastIndexOf: 11
 	}
