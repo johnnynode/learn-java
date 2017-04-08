5854fb8ae288c7827b1e0dc5fa02a0b7d7478c1b
johnnynode <johnnynode@gmail.com>
2017-04-08 15:43:15 +0800

    after format

--- src/chapter5/part01/Judge.java
+++ src/chapter5/part01/Judge.java
@@ -7,9 +7,12 @@ public class Judge {
 		String str1 = "aaa@";
 		String str2 = "aaaaa";
 		String str3 = "1111@111ffyu.dfg.com";
-		if(str1.matches(regx)) System.out.println("str1是一个合法的email地址");
-		if(str2.matches(regx)) System.out.println("str2是一个合法的email地址");
-		if(str3.matches(regx)) System.out.println("str3是一个合法的email地址"); // str3是一个合法的email地址
+		if (str1.matches(regx))
+			System.out.println("str1是一个合法的email地址");
+		if (str2.matches(regx))
+			System.out.println("str2是一个合法的email地址");
+		if (str3.matches(regx))
+			System.out.println("str3是一个合法的email地址"); // str3是一个合法的email地址
 	}
 
 }
